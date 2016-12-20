4a1e6d153c1b03d7c927f962ad40fbfaedafd3fe
Marcus Marohn <ma.marohn@gmail.com>
2016-12-20 14:20:34 +0100

    added raum view

--- RaumplanungAspNetCore/src/RaumplanungCore/Controllers/RaumController.cs
+++ RaumplanungAspNetCore/src/RaumplanungCore/Controllers/RaumController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Raumplanung.Database;
+using RaumplanungCore.Database;
+using RaumplanungCore.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +13,25 @@ namespace RaumplanungCore.Controllers
 {
     public class RaumController : Controller
     {
+        private readonly DatabaseHandler _databaseHandler;
+
+        public RaumController(ReservationContext context)
+        {
+            _databaseHandler = new DatabaseHandler(context);
+        }
+        
         // GET: /<controller>/
         public IActionResult Index()
         {
+            List<Room> rooms = _databaseHandler.GetAllRooms();
+            return View("Index", rooms);
+        }
 
+        // GET: /<controller>/Detail/reservationId
+        [HttpGet("Detail/{reservationId}")]
+        public IActionResult Detail(int reservationId)
+        {
+            ViewData["ReservationId"] = reservationId;
             return View();
         }
     }
