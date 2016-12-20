aa1a777bae2bce60ddc1e6cad0c845274dc7cacb
Jan <janmair@gmx.de>
2016-12-20 18:29:15 +0100

    Implemented a good chunk of the AuthentificationLogic

---
+++ RaumplanungAspNetCore/src/RaumplanungCore/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+
+namespace RaumplanungCore.Models
+{
+    public class ApplicationDbContext :IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=Reservation;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
