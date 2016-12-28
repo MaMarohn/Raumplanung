9484ab876706b19204bd231e3bb1d055661ddfb8
Marcus Marohn <ma.marohn@gmail.com>
2016-12-28 14:07:37 +0100

    added few gui elements, created class "Data" for all static data like school times, weekdays, etc.

---
+++ RaumplanungAspNetCore/src/RaumplanungCore/ViewModels/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RaumplanungCore.ViewModels
+{
+    public class Data
+    {
+        public static string[] DayStrings = { "Mo", "Di", "Mi", "Do", "Fr" };
+        public static string[] BlockStartArray = { "08:30", "10:15", "12:00", "14:15", "16:00", "17:45", "19:30" };
+        public static string[] BlockEndArray = { "10:00", "11:45", "13:30", "15:45", "17:30", "19:15", "21:00" };
+    }
+}
